5d2ad92a64803722c61192e5fddcc20066aacf74
Canghai Wang <canghaiw@student.unimelb.edu.au>
2025-03-06 10:17:02 +1100

    attempt trying gradient descent

---
+++ logistic regression classification/gradient_descent.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+#define learn_rate 0.0002
+
+
+void main(){
+    printf("hello world!");
+}
+
+
+void LR_gradient_descent(float t0, stepSize, f1,f2 , gradient, epislon){
+    float t = t0
+
+    int count = 0;
+
+    while(1){
+        count += 1;
+        t = 
+    }
+}
