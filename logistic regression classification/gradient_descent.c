54f129fb5809eabc42ced05d04ba70e11580b2b6
Canghai Wang <canghaiw@student.unimelb.edu.au>
2025-03-06 11:13:14 +1100

    sigmoid function

--- logistic regression classification/gradient_descent.c
+++ logistic regression classification/gradient_descent.c
@@ -1,12 +1,25 @@
 #include<stdio.h>
+#include<math.h>
 
 #define learn_rate 0.0002
 
+int x[10] = {31, 38, 48, 52, 63, 67, 75, 84, 89, 99}, y[10] = {553, 590, 608, 682, 752, 725, 834, 752, 845, 960};
 
-void main(){
-    printf("hello world!");
+float sigmoid_function(float x, float a, float b){
+    return 1/(1+exp(-(a*x+b)));
 }
 
+
+
+
+int main(){
+    float f;
+    f = sigmoid_function(10,1,1);
+    printf("sigmoid test: %f\n",f);
+    return 0;
+}
+
+/*
 
 void LR_gradient_descent(float t0, stepSize, f1,f2 , gradient, epislon){
     float t = t0
@@ -18,3 +31,10 @@ void LR_gradient_descent(float t0, stepSize, f1,f2 , gradient, epislon){
         t = 
     }
 }
+
+
+*/
+
+
+
+
